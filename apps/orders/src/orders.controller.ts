0b7790abaaed8a4438b608f1f3fe6bf76afc5bd1
josetomassilvaz <josetomassilvaz@gmail.com>
2024-05-26 18:48:52 -0400

    feat: :label: update types on controller

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Post, Body, Inject } from '@nestjs/common';
 import { ClientKafka } from '@nestjs/microservices';
 import { OrdersService } from './orders.service';
+import { IncomingOrder } from './types';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @Post()
-  async createSolicitud(@Body() order: any) {
+  async createSolicitud(@Body() order: IncomingOrder) {
     await this.ordersService.create(order);
     return order;
   }
